8b645b657b95d509002986ceeef0788489c722b7
DalminPai <dalmin.pai@cern.ch>
2018-07-07 17:17:16 +0900

    change sleep time from 5m to 1m

--- ICHEP2018/check.sh
+++ ICHEP2018/check.sh
@@ -42,10 +42,8 @@ do
 
 	./web.sh
 
-#	echo "(sleep 1m)"
-#	sleep 60
-	echo "(sleep 5m)"
-	sleep 300
+	echo "(sleep 1m)"
+	sleep 60
 	
 	echo "-------------------------------------------------------"
 done
